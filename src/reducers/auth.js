7cb3ad423302f965965c0bd089ac58f4e556b659
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-10 23:14:36 +0700

    Add auth reducer

---
+++ src/reducers/auth.js
@@ -0,0 +1,63 @@
+import {
+  SIGNUP_REQUEST,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILURE,
+  SIGNIN_REQUEST,
+  SIGNIN_SUCCESS,
+  SIGNIN_FAILURE,
+  LOGOUT
+} from '../constants/ActionTypes'
+
+const initialState = {
+  user: null,
+  loading: false,
+  isAuthenticated: false,
+  error: null
+}
+
+export default function authReducer (state = initialState, action) {
+  switch (action.type) {
+    case SIGNUP_REQUEST:
+      return {
+        ...state,
+        loading: true
+    }
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        loading: false
+    }
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.message
+      }
+    case SIGNIN_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case SIGNIN_SUCCESS:
+      return {
+        ...state,
+        user: action.payload.user,
+        loading: false,
+        isAuthenticated: true
+      }
+    case SIGNIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.message
+      }
+    case LOGOUT:
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false
+      }
+    default:
+      return state
+  }
+}
