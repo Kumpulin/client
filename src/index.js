eb4229f5f3674e1526a6c0d5caea923915219641
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-10 23:11:06 +0700

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 
-import App from './App'
+import store from './store'
+import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import rootReducer from '../reducers'
+
+const configureStore = preloadedState => {
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, createLogger())
+  )
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      store.replaceReducer(rootReducer)
+    })
+  }
+
+  return store
+}
+
+export default configureStore
