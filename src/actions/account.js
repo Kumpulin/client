57d2595504e8432471f977a7dc9aaa6d35724e4a
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-11 11:21:45 +0700

    Add action & rename some endpoint

--- src/actions/account.js
+++ src/actions/account.js
@@ -1,20 +1,27 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 import {
+  GET_CREATED_EVENTS,
   GET_JOINED_EVENTS,
   GET_ATTENDED_EVENTS,
 } from '../constants/ActionTypes'
 
+export const fetchCreatedEvents = ({ id }) => dispatch => {
+  axios.get(`/api/account/created_events`, {}, authHeader)
+    .then(({ data }) => dispatch(getAllCreatedEvents(data.events)))
+}
+
 export const fetchJoinedEvents = ({ id }) => dispatch => {
-  axios.get(`/api/account/joined`, {}, authHeader)
+  axios.get(`/api/account/joined_events`, {}, authHeader)
     .then(({ data }) => dispatch(getAllJoinedEvents(data.events)))
 }
 
 export const fetchAttendedEvents = ({ id }) => dispatch => {
-  axios.get(`/api/account/attended`, {}, authHeader)
+  axios.get(`/api/account/attended_events`, {}, authHeader)
     .then(({ data }) => dispatch(getAllAttendedEvents(data.events)))
 }
 
+const getAllCreatedEvents = events => ({ type: GET_CREATED_EVENTS, payload: { createdEvents: events } })
 const getAllJoinedEvents = events => ({ type: GET_JOINED_EVENTS, payload: { joinedEvents: events } })
 const getAllAttendedEvents = events => ({ type: GET_ATTENDED_EVENTS, payload: { attendedEvents: events } })
 
