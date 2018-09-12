b1c390e6cf09c6fbd9ab8d6171f19765a98f4d52
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 23:39:50 +0700

    Update MapPage.js

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 
 import MapContainer from '../containers/MapContainer'
 
-const MapPage = () => {
+const styles = {
+  page: {
+    position: 'fixed',
+    top: '0',
+    bottom: '0',
+    transform: 'translateX(50vw)',
+    height: '100%',
+    width: '100%',
+  }
+}
+
+function Page ({ classes }) {
   return (
-    <MapContainer />
+    <div className={classes.page}>
+      <MapContainer />
+    </div>
   )
 }
 
-export default MapPage
+Page.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Page)
