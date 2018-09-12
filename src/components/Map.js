4bc0447f41adbd892e4f5ab6f407adbc4ef1c24b
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 21:13:15 +0700

    Convert into stateless component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer'
@@ -7,42 +7,36 @@ const defaultOptions = {
   disableDefaultUI: true
 }
 
-class Map extends Component {
-  constructor (props) {
-    super(props)
-  }
-
-  render () {
-    const GoogleMapWithMarker = withScriptjs(withGoogleMap(props => (
-      <GoogleMap
-        defaultZoom={12}
-        defaultCenter={{ lat: -6.2297419  , lng: 106.759478 }}
-        defaultOptions={ defaultOptions }
+function Map ({ events }) {
+  const GoogleMapWithMarker = withScriptjs(withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: -6.2297419  , lng: 106.759478 }}
+      defaultOptions={ defaultOptions }
+    >
+      <MarkerClusterer
+        averageCenter
+        enableRetinaIcons
+        gridSize={60}
       >
-        <MarkerClusterer
-          averageCenter
-          enableRetinaIcons
-          gridSize={60}
-        >
-          {this.props.events.map((event, i) => (
-            <Marker
-              key={i}
-              position={{ lat: event.latitude  , lng: event.longitude }}
-            />
-          ))}
-        </MarkerClusterer>
-      </GoogleMap>
-    )))
+        {events.map((event, i) => (
+          <Marker
+            key={i}
+            position={{ lat: event.latitude  , lng: event.longitude }}
+          />
+        ))}
+      </MarkerClusterer>
+    </GoogleMap>
+  )))
 
-    return (
-      <GoogleMapWithMarker
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRjY_UwblVqO-J12K-9ZetbWYPS-gH1Ns&libraries=geometry"
-        loadingElement={ <div style={{ height: `100%` }} /> }
-        containerElement={ <div style={{ height: `100%` }}/> }
-        mapElement={ <div style={{ height: `100%` }}/> }
-      />
-    )
-  }
+  return (
+    <GoogleMapWithMarker
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRjY_UwblVqO-J12K-9ZetbWYPS-gH1Ns&libraries=geometry"
+      loadingElement={ <div style={{ height: `100%` }} /> }
+      containerElement={ <div style={{ height: `100%` }}/> }
+      mapElement={ <div style={{ height: `100%` }}/> }
+    />
+  )
 }
 
 Map.propTypes = {
