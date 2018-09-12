8d342830ca781034feed45f3347cf6f99d1edb5e
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 23:39:45 +0700

    Update LeftPage.js

--- src/pages/LeftPage.js
+++ src/pages/LeftPage.js
@@ -1,23 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 import { Route } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 
-import AboutUs from '../sections/AboutUs'
 import Team from '../sections/Team'
 import Home from '../sections/Home'
 
-const Page = () => {
+const styles = {
+  page: {
+    position: 'fixed',
+    top: '0',
+    bottom: '0',
+    height: '100%',
+    width: '100%',
+    transform: 'translateX(-50vw)',
+  }
+}
+
+function Page ({ classes }) {
   return (
-    <Grid container>
-      <Grid item xs={12} sm={6}>
-        <Route path="/team" component={Team}/>
-        <Route path="/about-us" component={AboutUs}/>
-      </Grid>
+    <Grid container className={classes.page} direction="row-reverse">
       <Grid item xs={12} sm={6}>
         <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Route path="/team" component={Team}/>
       </Grid>
     </Grid>
   )
 }
 
-export default Page
+Page.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Page)
