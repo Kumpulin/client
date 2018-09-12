0dc72686deb2f1f31a6121d526acaa30c2466fc8
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 13:44:13 +0700

    Add googleMapURL

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer'
 
 const defaultOptions = {
@@ -13,7 +13,7 @@ class Map extends Component {
   }
 
   render () {
-    const GoogleMapWithMarker = withGoogleMap(props => (
+    const GoogleMapWithMarker = withScriptjs(withGoogleMap(props => (
       <GoogleMap
         defaultZoom={12}
         defaultCenter={{ lat: -6.2297419  , lng: 106.759478 }}
@@ -24,7 +24,7 @@ class Map extends Component {
           enableRetinaIcons
           gridSize={60}
         >
-          {this.props.events.map((event, i) => (
+          {this.state.events.map((event, i) => (
             <Marker
               key={i}
               position={{ lat: event.latitude  , lng: event.longitude }}
@@ -32,10 +32,11 @@ class Map extends Component {
           ))}
         </MarkerClusterer>
       </GoogleMap>
-    ))
+    )))
 
     return (
       <GoogleMapWithMarker
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRjY_UwblVqO-J12K-9ZetbWYPS-gH1Ns&libraries=geometry"
         loadingElement={ <div style={{ height: `100%` }} /> }
         containerElement={ <div style={{ height: `100%` }}/> }
         mapElement={ <div style={{ height: `100%` }}/> }
