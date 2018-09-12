c793357d252f31986aab2e92c3cfd3fe0628fa70
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 13:53:34 +0700

    Add CssBaseline component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 import MapPage from '../pages/MapPage'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <MapPage />
-      </Router>
+      <Fragment>
+        <CssBaseline />
+        <Router>
+          <MapPage />
+        </Router>
+      </Fragment>
     )
   }
 }
