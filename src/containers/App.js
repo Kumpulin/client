0ff5d2cbd59aff67e0570663009b97d13866e86f
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 23:37:16 +0700

    Add button

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,34 @@
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import Button from '@material-ui/core/Button'
 
 import LeftPage from '../pages/LeftPage'
 import MapPage from '../pages/MapPage'
 
+const styles = theme => ({
+  startExploringButton: {
+    position: 'fixed',
+    left: '50%',
+    right: '50%',
+    transform: 'translate(-50%, -50%)',
+    bottom: theme.spacing.unit * 16,
+    width: theme.spacing.unit * 24,
+    color: '#ff5d5d',
+    backgroundColor: 'white',
+  }
+})
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+  }
+
   render() {
+    const { classes } = this.props
+
     return (
       <Fragment>
         <CssBaseline />
@@ -14,6 +36,7 @@ class App extends Component {
           <Fragment>
             <LeftPage />
             <MapPage />
+            <Button className={classes.startExploringButton} variant="extendedFab">Start Exploring!</Button>
           </Fragment>
         </Router>
       </Fragment>
@@ -21,4 +44,8 @@ class App extends Component {
   }
 }
 
-export default App
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(App)
