9454975b39afd2a2b939248b3fb87ac72d9b5067
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-13 02:05:02 +0700

    Show avatar on authenticated user

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
+import React from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/core/styles"
+import Avatar from "@material-ui/core/Avatar"
 
-import Map from '../components/Map'
+import Map from "../components/Map"
 
-const styles = {
+const styles = theme => ({
   page: {
-    position: 'fixed',
-    top: '0',
-    bottom: '0',
+    position: "fixed",
+    top: "0",
+    bottom: "0",
     transform: 'translateX(50vw)',
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%"
+  },
+  avatar: {
+    position: "fixed",
+    top: theme.spacing.unit * 4,
+    left: theme.spacing.unit * 4,
+    height: theme.spacing.unit * 6,
+    width: theme.spacing.unit * 6,
+    zIndex: 3
   }
-}
+})
 
-function Page ({ classes }) {
+function Page ({ classes, user }) {
   return (
     <div className={classes.page}>
+      {user !== null && <Avatar src={user.image} className={classes.avatar} />}
       <Map />
     </div>
   )
 }
 
 Page.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object
 }
 
-export default withStyles(styles)(Page)
+const mapStateToProps = state => ({
+  user: state.auth.user
+})
+
+export default withStyles(styles)(connect(mapStateToProps)(Page))
