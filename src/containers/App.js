1c96c3eededd986e8a4087e27ea718d028cc573f
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-12 11:55:29 +0700

    Add router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import MapPage from '../pages/MapPage'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Router>
+        <MapPage />
+      </Router>
     )
   }
 }
