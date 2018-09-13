662aa56f2a16b4dcf932c1df739f25fb1fe3ca53
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-13 21:04:44 +0700

    Add more app action

--- src/actions/app.js
+++ src/actions/app.js
@@ -1,7 +1,27 @@
 import {
-  SET_CURRENT_FULLPAGE
+  SET_CURRENT_FULLPAGE,
+  TOGGLE_SIGNUP_FORM,
+  TOGGLE_SIGNIN_FORM,
+  TOGGLE_FORGOT_PASSWORD_FORM,
+  TOGGLE_CHANGE_PASSWORD_FORM
 } from '../constants/ActionTypes'
 
 export function setCurrentFullPage (page) {
   return { type: SET_CURRENT_FULLPAGE, payload: { page } }
+}
+
+export function toggleSignUpForm (payload) {
+  return { type: TOGGLE_SIGNUP_FORM, payload }
+}
+
+export function toggleSignInForm (payload) {
+  return { type: TOGGLE_SIGNIN_FORM, payload }
+}
+
+export function toggleForgotPasswordForm (payload) {
+  return { type: TOGGLE_FORGOT_PASSWORD_FORM, payload }
+}
+
+export function toggleChangePasswordForm (payload) {
+  return { type: TOGGLE_CHANGE_PASSWORD_FORM, payload }
 }
