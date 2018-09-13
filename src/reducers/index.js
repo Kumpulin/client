988115fa59d1c09c902193b45c5007a1bbe6a100
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-13 18:21:58 +0700

    Combine app reducer with another reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 
+import app from './app'
 import auth from './auth'
 import account from './account'
 import event from './event'
 
 const rootReducer = combineReducers({
+  app,
   auth,
   account,
   event
