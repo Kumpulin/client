22b3deb9b96bc51aa48022c7439644ae11e554da
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-13 23:10:51 +0700

    Add back & create event button (not working)

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -3,8 +3,11 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import BackIcon from "@material-ui/icons/ArrowBack";
+import AddIcon from "@material-ui/icons/Add";
 import classNames from "classnames";
-import compose from "recompose/compose"
+import compose from "recompose/compose";
 
 import Map from "../components/Map";
 
@@ -17,7 +20,7 @@ const styles = theme => ({
     width: "100%",
     transition: theme.transitions.create(["transform"]),
     transform: "translateX(100vw)",
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up("lg")]: {
       transform: "translateX(50vw)"
     }
   },
@@ -34,7 +37,33 @@ const styles = theme => ({
     height: theme.spacing.unit * 6,
     width: theme.spacing.unit * 6,
     zIndex: 3
-  }
+  },
+  backButton: {
+    position: "fixed",
+    top: theme.spacing.unit * 4,
+    left: theme.spacing.unit * 4,
+    zIndex: 3,
+    backgroundColor: '#ff5d5d',
+    color: 'white',
+    boxShadow: theme.shadows[2],
+    transition: theme.transitions.create(['transform'])
+  },
+  hideBackButton: {
+    transform: `translateY(calc(-100% + -${theme.spacing.unit * 6}px))`
+  },
+  createEventFormButton: {
+    position: 'fixed',
+    bottom: theme.spacing.unit * 4,
+    right: theme.spacing.unit * 4,
+    zIndex: 3,
+    backgroundColor: '#ff5d5d',
+    color: 'white',
+    boxShadow: theme.shadows[2],
+    transition: theme.transitions.create(['transform'])
+  },
+  hidecreateEventFormButton: {
+    transform: `translateY(calc(100% + ${theme.spacing.unit * 6}px))`
+  },
 });
 
 function Page({ classes, user, currentFullPage }) {
@@ -46,7 +75,17 @@ function Page({ classes, user, currentFullPage }) {
         currentFullPage === "left" && classes.hidePage
       ])}
     >
-      {user !== null && <Avatar src={user.image} className={classes.avatar} />}
+      {/* {user !== null ? (
+        <Avatar src={user.image} className={classes.avatar} />
+      ) : (
+
+      )} */}
+      <Button className={classNames([classes.backButton, currentFullPage !== "map" && classes.hideBackButton])} variant="fab">
+        <BackIcon />
+      </Button>
+      <Button className={classNames([classes.createEventFormButton, currentFullPage !== "map" && classes.hidecreateEventFormButton])} variant="fab">
+        <AddIcon />
+      </Button>
       <Map />
     </div>
   );
@@ -63,4 +102,7 @@ const mapStateToProps = state => ({
   user: state.auth.user
 });
 
-export default compose(withStyles(styles), connect(mapStateToProps))(Page);
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps)
+)(Page);
