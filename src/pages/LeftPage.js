9efbb66e42318d652d6ead5e98f9de93ba9ede01
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-14 00:52:32 +0700

    Change to signle quotes

--- src/pages/LeftPage.js
+++ src/pages/LeftPage.js
@@ -1,36 +1,36 @@
-import React from "react"
-import { connect } from "react-redux"
-import PropTypes from "prop-types"
-import { withStyles } from "@material-ui/core/styles"
-import { Route } from "react-router-dom"
-import Grid from "@material-ui/core/Grid"
-import classNames from "classnames"
-import compose from "recompose/compose"
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import { Route } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import classNames from 'classnames'
+import compose from 'recompose/compose'
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth'
 
-import Team from "../sections/Team"
-import Home from "../sections/Home"
+import Team from '../sections/Team'
+import Home from '../sections/Home'
 
 const styles = theme => ({
   page: {
-    position: "fixed",
-    top: "0",
-    bottom: "0",
-    height: "100%",
-    width: "100%",
-    transition: theme.transitions.create(["transform"]),
-    [theme.breakpoints.down("lg")]: {
-      overflowY: "scroll"
+    position: 'fixed',
+    top: '0',
+    bottom: '0',
+    height: '100%',
+    width: '100%',
+    transition: theme.transitions.create(['transform']),
+    [theme.breakpoints.down('lg')]: {
+      overflowY: 'scroll'
     },
-    [theme.breakpoints.up("lg")]: {
-      transform: "translateX(-50vw)"
+    [theme.breakpoints.up('lg')]: {
+      transform: 'translateX(-50vw)'
     }
   },
   hidePage: {
-    transform: "translateX(-100vw)"
+    transform: 'translateX(-100vw)'
   },
   fullPage: {
-    transform: "translateX(0)"
+    transform: 'translateX(0)'
   }
 })
 
@@ -40,8 +40,8 @@ function Page({ classes, currentFullPage, width }) {
       container
       className={classNames([
         classes.page,
-        currentFullPage === "left" && classes.fullPage,
-        currentFullPage === "map" && classes.hidePage
+        currentFullPage === 'left' && classes.fullPage,
+        currentFullPage === 'map' && classes.hidePage
       ])}
       direction="row-reverse"
     >
@@ -49,13 +49,11 @@ function Page({ classes, currentFullPage, width }) {
         <Home />
       </Grid>
       <Grid item xs={12} lg={6}>
-        {
-          isWidthUp('lg', width) ? (
-            <Route path="/team" component={Team} />
-          ) : (
-            <Team />
-          )
-        }
+        {isWidthUp('lg', width) ? (
+          <Route path="/team" component={Team} />
+        ) : (
+          <Team />
+        )}
       </Grid>
     </Grid>
   )
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,37 +1,36 @@
-import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import BackIcon from "@material-ui/icons/ArrowBack";
-import AddIcon from "@material-ui/icons/Add";
-import classNames from "classnames";
-import compose from "recompose/compose";
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import BackIcon from '@material-ui/icons/ArrowBack'
+import AddIcon from '@material-ui/icons/Add'
+import classNames from 'classnames'
+import compose from 'recompose/compose'
 
-import Map from "../components/Map";
+import Map from '../components/Map'
 
 const styles = theme => ({
   page: {
-    position: "fixed",
-    top: "0",
-    bottom: "0",
-    height: "100%",
-    width: "100%",
-    transition: theme.transitions.create(["transform"]),
-    transform: "translateX(100vw)",
-    [theme.breakpoints.up("lg")]: {
-      transform: "translateX(50vw)"
+    position: 'fixed',
+    top: '0',
+    bottom: '0',
+    height: '100%',
+    width: '100%',
+    transition: theme.transitions.create(['transform']),
+    transform: 'translateX(100vw)',
+    [theme.breakpoints.up('lg')]: {
+      transform: 'translateX(50vw)'
     }
   },
   hidePage: {
-    transform: "translateX(100vw)"
+    transform: 'translateX(100vw)'
   },
   fullPage: {
-    transform: "translateX(0)"
+    transform: 'translateX(0)'
   },
   avatar: {
-    position: "fixed",
+    position: 'fixed',
     top: theme.spacing.unit * 4,
     left: theme.spacing.unit * 4,
     height: theme.spacing.unit * 6,
@@ -39,7 +38,7 @@ const styles = theme => ({
     zIndex: 3
   },
   backButton: {
-    position: "fixed",
+    position: 'fixed',
     top: theme.spacing.unit * 4,
     left: theme.spacing.unit * 4,
     zIndex: 3,
@@ -63,46 +62,53 @@ const styles = theme => ({
   },
   hidecreateEventFormButton: {
     transform: `translateY(calc(100% + ${theme.spacing.unit * 6}px))`
-  },
-});
+  }
+})
 
 function Page({ classes, user, currentFullPage }) {
   return (
     <div
       className={classNames([
         classes.page,
-        currentFullPage === "map" && classes.fullPage,
-        currentFullPage === "left" && classes.hidePage
+        currentFullPage === 'map' && classes.fullPage,
+        currentFullPage === 'left' && classes.hidePage
       ])}
     >
-      {/* {user !== null ? (
-        <Avatar src={user.image} className={classes.avatar} />
-      ) : (
-
-      )} */}
-      <Button className={classNames([classes.backButton, currentFullPage !== "map" && classes.hideBackButton])} variant="fab">
+      <Button
+        className={classNames([
+          classes.backButton,
+          currentFullPage !== 'map' && classes.hideBackButton
+        ])}
+        variant="fab"
+      >
         <BackIcon />
       </Button>
-      <Button className={classNames([classes.createEventFormButton, currentFullPage !== "map" && classes.hidecreateEventFormButton])} variant="fab">
+      <Button
+        className={classNames([
+          classes.createEventFormButton,
+          currentFullPage !== 'map' && classes.hidecreateEventFormButton
+        ])}
+        variant="fab"
+      >
         <AddIcon />
       </Button>
       <Map />
     </div>
-  );
+  )
 }
 
 Page.propTypes = {
   classes: PropTypes.object.isRequired,
   currentFullPage: PropTypes.string,
   user: PropTypes.object
-};
+}
 
 const mapStateToProps = state => ({
   currentFullPage: state.app.currentFullPage,
   user: state.auth.user
-});
+})
 
 export default compose(
   withStyles(styles),
   connect(mapStateToProps)
-)(Page);
+)(Page)
