97a1defec79510fa6ee5e6bf9b8df1a733d17eff
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-14 00:36:04 +0700

    Simplify input email, remove semicolon & change to single quotes

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,44 +1,43 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import Button from "@material-ui/core/Button";
-import { Typography } from "@material-ui/core";
-import emailValidator from "email-validator";
-import blue from "@material-ui/core/colors/blue";
-import classNames from "classnames";
-import { toggleSignUpForm, toggleSignInForm } from "../actions/app";
-import compose from "recompose/compose"
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import IconButton from '@material-ui/core/IconButton'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import FormControl from '@material-ui/core/FormControl'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+import Button from '@material-ui/core/Button'
+import { Typography, TextField } from '@material-ui/core'
+import emailValidator from 'email-validator'
+import classNames from 'classnames'
+import { toggleSignUpForm, toggleSignInForm } from '../actions/app'
+import compose from 'recompose/compose'
 
 const styles = theme => ({
   paper: {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    [theme.breakpoints.up("lg")]: {
-      left: "75%"
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    [theme.breakpoints.up('lg')]: {
+      left: '75%'
     },
-    transform: "translate(-50%, -50%)",
+    transform: 'translate(-50%, -50%)',
     padding: theme.spacing.unit * 4,
-    borderRadius: "10px",
+    borderRadius: '10px',
     width: theme.spacing.unit * 48,
-    transition: theme.transitions.create(["transform"])
+    transition: theme.transitions.create(['transform'])
   },
   form: {
-    display: "flex",
-    flexDirection: "column"
+    display: 'flex',
+    flexDirection: 'column'
   },
   formTitleGroup: {
-    textAlign: "center",
+    textAlign: 'center',
     marginBottom: theme.spacing.unit * 4
   },
   formTitle: {
@@ -50,75 +49,64 @@ const styles = theme => ({
   },
   buttonGroup: {
     marginTop: theme.spacing.unit * 4,
-    display: "flex",
-    justifyContent: "space-between"
+    display: 'flex',
+    justifyContent: 'space-between'
   },
   signUpButton: {
-    color: "#ff5d5d",
-    textTransform: "none",
+    color: '#ff5d5d',
+    textTransform: 'none',
     marginLeft: theme.spacing.unit * 2 * -1
   },
   signInButton: {
-    backgroundColor: "#ff5d5d",
-    color: "white",
-    boxShadow: "none",
-    transition: theme.transitions.create(["color", "background-color"]),
-    textTransform: "none"
-  },
-  cssLabel: {
-    "&$cssFocused": {
-      color: blue[500]
-    }
-  },
-  cssFocused: {},
-  cssUnderline: {
-    "&:after": {
-      borderBottomColor: blue[500]
-    }
+    backgroundColor: '#ff5d5d',
+    color: 'white',
+    boxShadow: 'none',
+    transition: theme.transitions.create(['color', 'background-color']),
+    textTransform: 'none'
   },
   hideForm: {
     transform: `translate(calc(100% + ${theme.spacing.unit * 24}px), -50%)`,
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up('lg')]: {
       transform: `translate(calc(75% + ${theme.spacing.unit * 24}px), -50%)`
     }
   }
-});
+})
 
 class SignInForm extends Component {
   state = {
-    email: "",
+    email: '',
     isEmailValid: true,
-    password: "",
+    password: '',
     showPassword: false
-  };
+  }
 
   handleEmailChange = event => {
     if (!emailValidator.validate(event.target.value)) {
-      this.setState({ isEmailValid: false });
+      this.setState({ isEmailValid: false })
     } else {
-      this.setState({ isEmailValid: true, email: event.target.value });
+      this.setState({ isEmailValid: true, email: event.target.value })
     }
-  };
+  }
 
   handlePasswordChange = event => {
-    this.setState({ password: event.target.value });
-  };
+    this.setState({ password: event.target.value })
+  }
 
   handleMouseDownPassword = event => {
-    event.preventDefault();
-  };
+    event.preventDefault()
+  }
 
   handleClickShowPassword = () => {
-    this.setState(state => ({ showPassword: !state.showPassword }));
-  };
+    this.setState(state => ({ showPassword: !state.showPassword }))
+  }
 
   handleSubmit() {
-    console.log(this.state);
+    console.log(this.state)
   }
 
   render() {
-    const { classes, showSignUpForm, showSignInForm } = this.props;
-    const { password, showPassword, isEmailValid } = this.state;
+    const { classes, showSignUpForm, showSignInForm } = this.props
+    const { password, showPassword, isEmailValid } = this.state
 
     return (
       <Paper
@@ -137,25 +125,12 @@ class SignInForm extends Component {
             </Typography>
           </div>
 
-          <FormControl error={!isEmailValid}>
-            <InputLabel
-              FormLabelClasses={{
-                root: classes.cssLabel,
-                focused: classes.cssFocused
-              }}
-            >
-              Email
-            </InputLabel>
-            <Input
-              classes={{
-                underline: classes.cssUnderline
-              }}
-              onChange={this.handleEmailChange}
-            />
-            <FormHelperText>
-              {!isEmailValid && "Invalid email address."}
-            </FormHelperText>
-          </FormControl>
+          <TextField
+            error={!isEmailValid}
+            label="Email"
+            onChange={this.handleEmailChange}
+            helperText={!isEmailValid ? 'Invalid email address.': ' '}
+          />
 
           <FormControl className={classes.textFieldWithMarginTop}>
             <InputLabel
@@ -170,7 +145,7 @@ class SignInForm extends Component {
               classes={{
                 underline: classes.cssUnderline
               }}
-              type={showPassword ? "text" : "password"}
+              type={showPassword ? 'text' : 'password'}
               value={password}
               onChange={this.handlePasswordChange}
               endAdornment={
@@ -196,25 +171,25 @@ class SignInForm extends Component {
           </div>
         </form>
       </Paper>
-    );
+    )
   }
 }
 
 SignInForm.propTypes = {
   classes: PropTypes.object.isRequired,
   showSignUpForm: PropTypes.func.isRequired
-};
+}
 
 const mapStateToProp = state => ({
   showSignInForm: state.app.isSignIn
-});
+})
 
 const mapDispatchToProps = dispatch => ({
   showSignUpForm: () => {
-    dispatch(toggleSignInForm(false));
-    dispatch(toggleSignUpForm(true));
+    dispatch(toggleSignInForm(false))
+    dispatch(toggleSignUpForm(true))
   }
-});
+})
 
 export default compose(
   connect(
@@ -222,4 +197,4 @@ export default compose(
     mapDispatchToProps
   ),
   withStyles(styles)
-)(SignInForm);
+)(SignInForm)
