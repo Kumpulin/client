8895c73a0e2d9e881cc2161cee7b17dd3b2086c9
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-15 20:26:21 +0700

    Move Google Maps Javascript API v3 url to index.html

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,41 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer'
 
 const defaultOptions = {
   disableDefaultUI: true
 }
 
-function Map ({ events }) {
-  const GoogleMapWithMarker = withScriptjs(withGoogleMap(props => (
+function Map({ events }) {
+  const GoogleMapWithMarker = withGoogleMap(props => (
     <GoogleMap
       defaultZoom={12}
-      defaultCenter={{ lat: -6.2297419  , lng: 106.759478 }}
-      defaultOptions={ defaultOptions }
+      defaultCenter={{ lat: -6.2297419, lng: 106.759478 }}
+      defaultOptions={defaultOptions}
     >
-      <MarkerClusterer
-        averageCenter
-        enableRetinaIcons
-        gridSize={60}
-      >
+      <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
         {events.map((event, i) => (
           <Marker
             key={i}
-            position={{ lat: event.latitude  , lng: event.longitude }}
+            position={{ lat: event.latitude, lng: event.longitude }}
           />
         ))}
       </MarkerClusterer>
     </GoogleMap>
-  )))
+  ))
 
   return (
     <GoogleMapWithMarker
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRjY_UwblVqO-J12K-9ZetbWYPS-gH1Ns&libraries=geometry"
-      loadingElement={ <div style={{ height: `100%` }} /> }
-      containerElement={ <div style={{ height: `100%` }}/> }
-      mapElement={ <div style={{ height: `100%` }}/> }
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `100%` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
     />
   )
 }
