6444e2ce262236b6d6d69a7ac085b250641a7580
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-18 14:22:39 +0700

    Fetch events after map mounted

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,38 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer'
+import { fetchAllEvents } from '../actions/event'
 
 const defaultOptions = {
   disableDefaultUI: true
 }
 
-function Map({ events }) {
-  const GoogleMapWithMarker = withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: -6.2297419, lng: 106.759478 }}
-      defaultOptions={defaultOptions}
-    >
-      <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
-        {events.map((event, i) => (
-          <Marker
-            key={i}
-            position={{ lat: event.latitude, lng: event.longitude }}
-          />
-        ))}
-      </MarkerClusterer>
-    </GoogleMap>
-  ))
+class Map extends Component {
+  componentDidMount() {
+    this.props.fetchAllEvents()
+  }
 
-  return (
-    <GoogleMapWithMarker
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100%` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
-  )
+  render() {
+    const { events } = this.props
+
+    const GoogleMapWithMarker = withGoogleMap(props => (
+      <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{ lat: -6.2297419, lng: 106.759478 }}
+        defaultOptions={defaultOptions}
+      >
+        <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
+          {events.map((event, i) => (
+            <Marker
+              key={i}
+              position={{
+                lat: JSON.parse(event.latitude),
+                lng: JSON.parse(event.longitude)
+              }}
+            />
+          ))}
+        </MarkerClusterer>
+      </GoogleMap>
+    ))
+
+    return (
+      <GoogleMapWithMarker
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    )
+  }
 }
 
 Map.propTypes = {
@@ -43,4 +55,11 @@ const mapStateToProps = state => ({
   events: state.event.events
 })
 
-export default connect(mapStateToProps)(Map)
+const mapDispatchToProps = dispatch => ({
+  fetchAllEvents: () => dispatch(fetchAllEvents())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Map)
