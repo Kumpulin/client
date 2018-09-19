36f436c254749ecdbd9e610f0aa84f793e21593f
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-19 11:06:58 +0700

    Replace hardcoded api url with .env variable

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
 
-axios.defaults.baseURL = 'http://localhost:8081'
+axios.defaults.baseURL = process.env.REACT_APP_KUMPULIN_API_URL
 
 const store = configureStore()
 
