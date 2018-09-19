2671d279a73e2078646ed07e420697fe551e6cd6
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-19 07:41:09 +0700

    Remove dropdown with button

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,14 +1,11 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link, NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
-import Avatar from '@material-ui/core/Avatar'
 import grey from '@material-ui/core/colors/grey'
 import Hidden from '@material-ui/core/Hidden'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
 import { setCurrentFullPage, toggleSignInForm } from '../actions/app'
 import { logout } from '../actions/auth'
 import compose from 'recompose/compose'
@@ -73,84 +70,54 @@ const styles = theme => ({
   }
 })
 
-class Home extends Component {
-  state = {
-    anchorEl: null
-  }
+function Home(props) {
+  const {
+    classes,
+    user,
+    setLeftPageFull,
+    showSignInForm,
+    backToLandingPage,
+    logout
+  } = props
 
-  handleClick = event => {
-    this.setState({ anchorEl: event.currentTarget })
-  }
-
-  handleLogoutClick = () => {
-    const { dispatch } = this.props
-
-    dispatch(logout())
-
-    this.setState({ anchorEl: null })
-  }
-
-  render() {
-    const {
-      classes,
-      user,
-      setLeftPageFull,
-      showSignInForm,
-      backToLandingPage
-    } = this.props
-
-    const { anchorEl } = this.state
-
-    return (
-      <div className={classes.home}>
-        <header className={classes.header}>
-          <div className={classes.navbar}>
-            <h1 className={classes.logo} onClick={backToLandingPage}>
-              <Link to="/">Kumpulin</Link>
-            </h1>
-            <Hidden only="xs">
-              <NavLink
-                onClick={setLeftPageFull}
-                className={classes.link}
-                activeClassName={classes.linkActive}
-                to="/team"
-              >
-                Team
-              </NavLink>
-            </Hidden>
-          </div>
-          {user !== null ? (
-            <Fragment>
-              <Avatar
-                className={classes.avatar}
-                src={user.image}
-                alt={user.displayName !== null ? user.displayName : ''}
-                onClick={this.handleClick}
-                aria-owns={anchorEl ? 'simple-menu' : null}
-                aria-haspopup="true"
-              />
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                open={Boolean(anchorEl)}
-                onClose={this.handleClose}
-              >
-                <MenuItem onClick={this.handleLogoutClick}>Logout</MenuItem>
-              </Menu>
-            </Fragment>
-          ) : (
-            <Button
-              variant="extendedFab"
-              className={classes.signInButton}
-              onClick={showSignInForm}
+  return (
+    <div className={classes.home}>
+      <header className={classes.header}>
+        <div className={classes.navbar}>
+          <h1 className={classes.logo} onClick={backToLandingPage}>
+            <Link to="/">Kumpulin</Link>
+          </h1>
+          <Hidden only="xs">
+            <NavLink
+              onClick={setLeftPageFull}
+              className={classes.link}
+              activeClassName={classes.linkActive}
+              to="/team"
             >
-              Sign In
-            </Button>
-          )}
-        </header>
-      </div>
-    )
-  }
+              Team
+            </NavLink>
+          </Hidden>
+        </div>
+        {user !== null ? (
+          <Button
+            variant="extendedFab"
+            className={classes.signInButton}
+            onClick={logout}
+          >
+            Logout
+          </Button>
+        ) : (
+          <Button
+            variant="extendedFab"
+            className={classes.signInButton}
+            onClick={showSignInForm}
+          >
+            Sign In
+          </Button>
+        )}
+      </header>
+    </div>
+  )
 }
 
 Home.propTypes = {
@@ -169,6 +136,7 @@ const mapDispatchToProps = dispatch => ({
   backToLandingPage: () => dispatch(setCurrentFullPage(null)),
   setLeftPageFull: () => dispatch(setCurrentFullPage('left')),
   showSignInForm: () => dispatch(toggleSignInForm(true)),
+  logout: () => dispatch(logout()),
   dispatch
 })
 
