551c65c083fbb8232433374c6fac4836ef39294c
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2018-09-21 13:34:11 +0700

    Return state to default on logout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,56 @@ import auth from './auth'
 import account from './account'
 import event from './event'
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   app,
   auth,
   account,
   event
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === 'LOGOUT') {
+    state = {
+      app: {
+        activeStep: 0,
+        currentFullPage: null,
+        isSignUp: false,
+        isSignIn: false,
+        isForgotPassword: false,
+        isChangePassword: false,
+        isCreateEvent: false,
+        isMarkerClicked: false,
+        isUserImageClicked: false
+      },
+      auth: {
+        user: null,
+        loading: false,
+        isAuthenticated: false,
+        error: null
+      },
+      account: {
+        joinedEvents: [],
+        attendedEvents: [],
+        newPassword: null
+      },
+      event: {
+        events: state.event.events,
+        currentEvent: null,
+        currentEventDetails: null,
+        loading: false,
+        error: null,
+        isUpdateEvent: false,
+        temp: {
+          isImageChange: false,
+          eventDetails: null,
+          eventImage: null,
+          eventAdditionalSettings: null
+        }
+      }
+    }
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer
